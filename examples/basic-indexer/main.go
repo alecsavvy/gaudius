622ba843663423f8de06947713a99081f3548c4f
Alec Savvy <alecjsavvy@gmail.com>
2023-11-08 22:28:34 -0700

    basic indexer

--- examples/basic-indexer/main.go
+++ examples/basic-indexer/main.go
@@ -1,6 +1,54 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/alecsavvy/gaudius/gen/contracts"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
 func main() {
-	gaudius.
-	fmt.Println("starting indexer")
+	ctx := context.Background()
+
+	addr := common.HexToAddress("0x1Cd8a543596D499B9b6E7a6eC15ECd2B7857Fd64")
+	cl, err := ethclient.Dial("https://acdc-gateway.audius.co/")
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	em, err := contracts.NewEntityManager(addr, cl)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	start := uint64(20605697)
+	current := start
+
+	for true {
+		opts := &bind.FilterOpts{
+			Start: current,
+			End: &current,
+			Context: ctx,
+		}
+	
+		eventiter, err := em.FilterManageEntity(opts)
+		defer eventiter.Close()
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		for eventiter.Next() {
+			event := eventiter.Event
+			fmt.Println(event)
+		}
+
+		current += 1
+	}
 }
